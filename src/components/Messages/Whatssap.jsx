663a9bc311a4abd85cb9edcc842d302d3407f66c
Sofiaspinelli <sofiaspinelli17@gmail.com>
2024-04-18 23:16:46 -0300

    fix change number

--- src/components/Messages/Whatssap.jsx
+++ src/components/Messages/Whatssap.jsx
@@ -3,7 +3,7 @@ import './whatssap.css'
 import { FaWhatsapp } from "react-icons/fa";
 const whatsapp = () => {
   return (
-    <a href="https://api.whatsapp.com/send?phone=54911654508412" className="float" target="_blank">
+    <a href="https://api.whatsapp.com/send?phone=5491165450841" className="float" target="_blank">
         <FaWhatsapp className='my-float'/>
     </a>
   )
